c0a18b0fce2e1e1786079bd4aaa4fb791dc428d6
Richard Ma <richard.ma.19850509@gmail.com>
2018-10-18 16:56:25 +0800

    add

---
+++ dive-into-python3/Chapter2/list.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+if __name__ == '__main__':
+    # slice
+    a_list = ['a', 'b', 'mpilgrim', 'z', 'example']
+    print(a_list[1:3])
+    print(a_list[1:-1])
+    print(a_list[0:3])
+    print(a_list[:3])
+    print(a_list[3:])
+    print(a_list[:])
+
+    # add
+    a_list = ['a']
+    print(a_list + [2.0, 3])
+    a_list.append(True)
+    print(a_list)
+    a_list.extend(['four', 'omega'])
+    print(a_list)
+    a_list.insert(0, 'ooo')
+    print(a_list)
+
+    # search
+    a_list = ['a', 'b', 'new', 'mpilgrim', 'new']
+    print(a_list.count('new'))
+    print(a_list.index('mpilgrim'))
+
+    # remove
+    a_list = ['a', 'b', 'new', 'mpilgrim', 'new']
+    del a_list[1]
+    print(a_list)
+
+    a_list.remove('new')
+    print(a_list)
+
+    print(a_list)
+    print(a_list.pop())
+    print(a_list)
+    print(a_list.pop(0))
+    print(a_list)
---
+++ dive-into-python3/Chapter2/math.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import math
+
+if __name__ == '__main__':
+    print(math.pi)
+    print(math.sin(math.pi / 2))
+    print(math.tan(math.pi / 4))
