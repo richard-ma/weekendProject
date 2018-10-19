72ae313f74950e5bb047185e8fb2f26b2152fd31
Richard Ma <richard.ma.19850509@gmail.com>
2018-10-19 12:35:23 +0800

    use unittest

--- dive-into-python3/Chapter2/math.py
+++ dive-into-python3/Chapter2/math.py
@@ -3,7 +3,22 @@
 
 import math
 
+import unittest
+
+class TestMath(unittest.TestCase):
+
+    """Test case docstring."""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_name(self):
+        self.assertEqual(3.141592653589793, math.pi)
+        self.assertEqual(1.0, math.sin(math.pi / 2))
+        self.assertEqual(0.9999999999999999, math.tan(math.pi / 4))
+
 if __name__ == '__main__':
-    print(math.pi)
-    print(math.sin(math.pi / 2))
-    print(math.tan(math.pi / 4))
+    unittest.main()
