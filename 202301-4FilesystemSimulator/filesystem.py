38949226046118ebae746c54167bc0c09d593290
richard-ma <richard.ma.19850509@gmail.com>
2023-01-22 16:04:52 +0800

    add write and read method

--- 202301-4FilesystemSimulator/filesystem.py
+++ 202301-4FilesystemSimulator/filesystem.py
@@ -6,11 +6,16 @@ from file import File
 class Filesystem:
     DATA_FILENAME = 'data.dat'
 
+    BLOCK_UNUSED = 0
+    BLOCK_USED = 1
+
     def __init__(self, block_size=1024, block_count=1024):
         self._block_size = block_size
         self._block_count = block_count
         self._bitmap = [0] * self._block_count
-        self._disk = {k: Block(self._block_size) for k in range(self._block_count)}
+        self._next_size = len(str(self._block_count))
+        self._data_size = self._block_size - self._next_size
+        self._disk = {k: Block(self._block_size, self._next_size) for k in range(self._block_count)}
         '''
         # setting block 0: root Directory /
         root_dir = Directory()
@@ -24,7 +29,7 @@ class Filesystem:
         '''
 
     def _is_block_used(self, idx: int):
-        if idx < self._block_count and self._bitmap[idx] == 1:
+        if idx < self._block_count and self._bitmap[idx] == Filesystem.BLOCK_USED:
             return True
         else:
             return False
@@ -34,6 +39,55 @@ class Filesystem:
             return self._disk[idx]
         else:
             raise IndexError("Block index out of [size: {:d}] {:d}".format(self._block_count, idx))
+
+    def _write_block(self, idx: int, data: str, next_block_id=0):
+        if not self._is_block_used(idx):
+            self._disk[idx].write(data, next_block_id)
+            self._bitmap[idx] = Filesystem.BLOCK_USED 
+        else:
+            raise Exception("Block {:d} is used.".format(idx))
+
+    def _malloc_one_block(self, start=0):
+        for idx in range(start+1, self._block_count):
+            if not self._is_block_used(idx): # success
+                return idx
+
+        # failed
+        raise Exception("Not enough disk space.")
+
+
+    def write(self, data: str):
+        size = len(data)
+
+        block_data = data[:self._data_size]
+        data = data[self._data_size:]
+
+        file_block_id = block_id = self._malloc_one_block()
+
+        while len(data) > 0:
+            new_block_id = self._malloc_one_block(block_id)
+
+            self._write_block(block_id, block_data, new_block_id)
+
+            block_data = data[:self._data_size]
+            data = data[self._data_size:]
+            block_id = new_block_id
+
+        self._write_block(block_id, block_data)
+
+        return file_block_id
+
+    def read(self, idx: int):
+        data = ""
+
+        block = self._get_block(idx) # first block
+        data += block.read()
+
+        while block.has_next_block():
+            block = self._get_block(block.get_next_block()) # update block to next block
+            data += block.read()
+
+        return data
 
     '''
     def malloc(self, size: int):
@@ -113,6 +167,7 @@ class Filesystem:
 
 if __name__ == "__main__":
     fs = Filesystem()
+    '''
     fs.load()
     block_id = 33
     data = "hello"
@@ -121,3 +176,13 @@ if __name__ == "__main__":
     assert fs.get_pwd().get_name() == '/' # root directory name is /
     assert fs.get_pwd().get_current_block() == 0 # root directory block id is 0
     fs.quit()
+    '''
+    # test one block data
+    data = "zkzk"*4
+    block_id = fs.write(data)
+    assert data == fs.read(block_id)
+
+    # test multi blocks data
+    data = "zkzk"*1000
+    block_id = fs.write(data)
+    assert data == fs.read(block_id)
