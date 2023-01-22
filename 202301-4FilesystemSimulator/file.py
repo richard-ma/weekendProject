20303d6b5239202b88cc6ee46436e34b6ad6e170
richard-ma <richard.ma.19850509@gmail.com>
2023-01-22 12:50:22 +0800

    use File Class

--- 202301-4FilesystemSimulator/file.py
+++ 202301-4FilesystemSimulator/file.py
@@ -1,8 +1,20 @@
-class AbstrackFile:
+class File:
+    TYPE_DIRECTORY = 0
+    TYPE_FILE = 1
+
+
     def __init__(self):
-        self._current_block = None
+        self._type = None
         self._name = ""
+        self._current_block = None
         self._parent_block = None
+        self._children_block = list()
+
+    def set_type(self, t: int):
+        if t not in [File.TYPE_DIRECTORY, File.TYPE_FILE]:
+            raise TypeError("Such file type not found.")
+
+        self._type = t
 
     def set_name(self, name: str):
         self._name = name
@@ -10,76 +22,31 @@ class AbstrackFile:
     def get_name(self):
         return self._name
 
-    def is_file(self):
-        return isinstance(self, File)
+    def is_type(self, t: int):
+        if t not in [File.TYPE_DIRECTORY, File.TYPE_FILE]:
+            raise TypeError("Such file type not found.")
 
-    def is_dir(self):
-        return isinstance(self, Directory)
+        return self._type == t
 
     def set_current_block(self, block_id: int):
-        self._current_block = str(block_id)
+        self._current_block = block_id
 
     def get_current_block(self):
-        return int(self._current_block)
+        return self._current_block
 
     def get_parent_block(self):
-        return int(self._parent_block)
+        return self._parent_block
 
     def set_parent_block(self, block_id: int):
-        self._parent_block = str(block_id)
-
-    def __str__(self):
-        return ';'.join([str(k)+":"+str(v) for k,v in self.__dict__.items()])
-
-    def load_from_block(self, s: str):
-        for item in s.split(";"):
-            k, v = item.split(":")
-            self.__dict__[k] = v
-
-
-class File(AbstrackFile):
-    def __init__(self):
-        super().__init__()
-        self._data_blocks = list()
-
-    def _add_data_block(self, block_id: int):
-        self._data_blocks.append(str(block_id))
-
-
-class Directory(AbstrackFile):
-    def __init__(self):
-        super().__init__()
-        self._children_block = list()
+        self._parent_block = block_id
 
     def add_child(self, block_id: int):
         self._children_block.append(item)
 
-    def get_all_children_block_id(self):
+    def get_all_children_block(self):
         return self._children_block
 
 
 if __name__ == "__main__":
     file_name = "file.file"
     dire_name = "dire"
-
-    f = File()
-    f.set_name(file_name)
-    assert f.get_name() == file_name
-    assert f.is_dir() is False
-    assert f.is_file() is True
-    new_f = File()
-    new_f.load_from_block(str(f))
-    assert str(new_f) == str(f)
-
-    d = Directory()
-    d.set_name(dire_name)
-    assert d.get_name() == dire_name
-    assert d.is_dir() is True
-    assert d.is_file() is False
-    new_d = Directory()
-    new_d.load_from_block(str(d))
-    assert str(new_d) == str(d)
-
-    af = AbstrackFile()
-    assert af.is_dir() is False
-    assert af.is_file() is False
