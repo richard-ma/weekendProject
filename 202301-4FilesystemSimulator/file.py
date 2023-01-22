ef2f3b2e1459a4a16318eb45465643a3c0b27ccc
richard-ma <richard.ma.19850509@gmail.com>
2023-01-22 13:34:06 +0800

    add unit test

--- 202301-4FilesystemSimulator/file.py
+++ 202301-4FilesystemSimulator/file.py
@@ -41,12 +41,57 @@ class File:
         self._parent_block = block_id
 
     def add_child(self, block_id: int):
-        self._children_block.append(item)
+        self._children_block.append(block_id)
 
     def get_all_children_block(self):
         return self._children_block
 
 
 if __name__ == "__main__":
-    file_name = "file.file"
-    dire_name = "dire"
+    file_data = {
+        "type": File.TYPE_FILE,
+        "name": "file.file",
+        "current_block": 33,
+        "parent_block": 0,
+        "children_blocks": [],
+        "data_buffer": "hello this is my first file." # TODO: add buffer for File
+    }
+
+    dire_data = {
+        "type": File.TYPE_DIRECTORY,
+        "name": "dire",
+        "current_block": 25,
+        "parent_block": 0,
+        "children_blocks": [33, 24],
+        "data_buffer": ""
+    }
+
+    f = File()
+    f.set_type(file_data["type"])
+    f.set_name(file_data["name"])
+    f.set_current_block(file_data["current_block"])
+    f.set_parent_block(file_data["parent_block"])
+    for block_id in file_data['children_blocks']:
+        f.add_child(block_id)
+
+    d = File()
+    d.set_type(dire_data["type"])
+    d.set_name(dire_data["name"])
+    d.set_current_block(dire_data["current_block"])
+    d.set_parent_block(dire_data["parent_block"])
+    for block_id in dire_data['children_blocks']:
+        d.add_child(block_id)
+
+    assert f.is_type(File.TYPE_FILE) is True
+    assert f.is_type(File.TYPE_DIRECTORY) is False
+    assert f.get_name() == file_data["name"]
+    assert f.get_current_block() == file_data["current_block"]
+    assert f.get_parent_block() == file_data["parent_block"]
+    assert f.get_all_children_block() == file_data["children_blocks"]
+
+    assert d.is_type(File.TYPE_FILE) is False
+    assert d.is_type(File.TYPE_DIRECTORY) is True
+    assert d.get_name() == dire_data["name"]
+    assert d.get_current_block() == dire_data["current_block"]
+    assert d.get_parent_block() == dire_data["parent_block"]
+    assert d.get_all_children_block() == dire_data["children_blocks"]
