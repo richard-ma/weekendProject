429a91c713d497865bde7766c3c089f5ad783b12
richard-ma <richard.ma.19850509@gmail.com>
2023-01-22 15:35:05 +0800

    move next_legnth to filesystem

--- 202301-4FilesystemSimulator/block.py
+++ 202301-4FilesystemSimulator/block.py
@@ -1,17 +1,15 @@
 class Block:
-    def __init__(self, size: int):
+    def __init__(self, size: int, next_length: int):
         self._size = size
         self._data = ""
 
         self._next = None
+        self._next_length = next_length
 
-    @property
-    def _next_legnth(self):
-        return len(str(self._size))
 
     @property
     def _data_length(self):
-        return self._size - self._next_legnth
+        return self._size - self._next_length
 
     def write(self, data: str, next_block_id=0):
         if len(data) > self._data_length:
@@ -36,8 +34,8 @@ class Block:
         if len(s) > self._size:
             raise Exception("Too long for [size: {:d}]: {:s}".format(self._size, s))
 
-        self._data = s[:-self._next_legnth]
-        self._next = int(s[-self._next_legnth:])
+        self._data = s[:-self._next_length]
+        self._next = int(s[-self._next_length:])
 
 
 if __name__ == "__main__":
@@ -45,16 +43,17 @@ if __name__ == "__main__":
         "size": 10,
         "data": "23"*2,
         "next_block_id": 33,
+        "next_length": 4
     }
     block_data["next_length"] = len(str(block_data["size"]))
 
 
-    b = Block(block_data["size"])
+    b = Block(block_data["size"], block_data["next_length"])
     b.write(block_data["data"])
 
     assert b.read() == block_data["data"]
-    assert b._next_legnth == block_data["next_length"]
-    assert b._data_length == block_data["size"] - b._next_legnth
+    assert b._next_length == block_data["next_length"]
+    assert b._data_length == block_data["size"] - b._next_length
     assert b.has_next_block() is False
 
     b.write(block_data["data"], next_block_id=block_data["next_block_id"])
@@ -66,7 +65,7 @@ if __name__ == "__main__":
     b.load(s)
 
     assert b.read() == block_data["data"]
-    assert b._next_legnth == block_data["next_length"]
-    assert b._data_length == block_data["size"] - b._next_legnth
+    assert b._next_length == block_data["next_length"]
+    assert b._data_length == block_data["size"] - b._next_length
     assert b.get_next_block() == block_data["next_block_id"]
     assert b.has_next_block() is True
