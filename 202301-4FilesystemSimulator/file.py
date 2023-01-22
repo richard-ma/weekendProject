6e8fe8b28537b904e551707edd93e9815368d890
richard-ma <richard.ma.19850509@gmail.com>
2023-01-22 14:44:35 +0800

    add save and load

--- 202301-4FilesystemSimulator/file.py
+++ 202301-4FilesystemSimulator/file.py
@@ -9,6 +9,7 @@ class File:
         self._current_block = None
         self._parent_block = None
         self._children_block = list()
+        self._data_buffer = ""
 
     def set_type(self, t: int):
         if t not in [File.TYPE_DIRECTORY, File.TYPE_FILE]:
@@ -46,6 +47,41 @@ class File:
     def get_all_children_block(self):
         return self._children_block
 
+    def save(self):
+        children_list = list(map(str, self._children_block))
+        children_list = ','.join(children_list)
+        head = [
+            self._type,
+            self._name,
+            self._current_block,
+            self._parent_block,
+            children_list,
+        ]
+        head = ":".join(list(map(str, head)))
+        body = self._data_buffer
+
+        return ";;".join([head, body])
+
+    def load(self, s: str):
+        head, body = s.split(";;")
+        self._data_buffer = body
+
+        head = head.split(":")
+        self._type = int(head[0])
+        self._name = head[1]
+        self._current_block = int(head[2])
+        self._parent_block = int(head[3])
+
+        # deal with empty head[4]
+        if "," in head[4]:
+            self._children_block = list(head[4].split(","))
+            for idx in range(len(self._children_block)):
+                self._children_block[idx] = int(self._children_block[idx])
+        elif len(head[4]) == 0: # empty head[4]
+            self._children_block = list()
+        else: # only one item in children_block
+            self._children_block = [int(head[4])]
+
 
 if __name__ == "__main__":
     file_data = {
@@ -53,7 +89,7 @@ if __name__ == "__main__":
         "name": "file.file",
         "current_block": 33,
         "parent_block": 0,
-        "children_blocks": [],
+        "children_blocks": list(),
         "data_buffer": "hello this is my first file." # TODO: add buffer for File
     }
 
@@ -82,16 +118,29 @@ if __name__ == "__main__":
     for block_id in dire_data['children_blocks']:
         d.add_child(block_id)
 
-    assert f.is_type(File.TYPE_FILE) is True
-    assert f.is_type(File.TYPE_DIRECTORY) is False
-    assert f.get_name() == file_data["name"]
-    assert f.get_current_block() == file_data["current_block"]
-    assert f.get_parent_block() == file_data["parent_block"]
-    assert f.get_all_children_block() == file_data["children_blocks"]
+    def file_assertions(f: "File", file_data):
+        assert f.is_type(File.TYPE_FILE) is True
+        assert f.is_type(File.TYPE_DIRECTORY) is False
+        assert f.get_name() == file_data["name"]
+        assert f.get_current_block() == file_data["current_block"]
+        assert f.get_parent_block() == file_data["parent_block"]
+        assert f.get_all_children_block() == file_data["children_blocks"]
 
-    assert d.is_type(File.TYPE_FILE) is False
-    assert d.is_type(File.TYPE_DIRECTORY) is True
-    assert d.get_name() == dire_data["name"]
-    assert d.get_current_block() == dire_data["current_block"]
-    assert d.get_parent_block() == dire_data["parent_block"]
-    assert d.get_all_children_block() == dire_data["children_blocks"]
+    def dire_assertions(d: "File", dire_data):
+        assert d.is_type(File.TYPE_FILE) is False
+        assert d.is_type(File.TYPE_DIRECTORY) is True
+        assert d.get_name() == dire_data["name"]
+        assert d.get_current_block() == dire_data["current_block"]
+        assert d.get_parent_block() == dire_data["parent_block"]
+        assert d.get_all_children_block() == dire_data["children_blocks"]
+
+    file_assertions(f, file_data)
+    dire_assertions(d, dire_data)
+
+    s = f.save()
+    f.load(s)
+    file_assertions(f, file_data)
+
+    s = d.save()
+    d.load(s)
+    dire_assertions(d, dire_data)
