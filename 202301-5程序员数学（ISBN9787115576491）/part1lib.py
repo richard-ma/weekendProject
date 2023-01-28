a0b8697a6b1df475e3e9d204444a67dd435f664b
richard-ma <richard.ma.19850509@gmail.com>
2023-01-28 18:09:25 +0800

    添加了对无解方程组的处理

--- 202301-5程序员数学（ISBN9787115576491）/part1lib.py
+++ 202301-5程序员数学（ISBN9787115576491）/part1lib.py
@@ -44,19 +44,22 @@ def intersection(u1, u2, v1, v2):
     a2, b2, c2 = standard_form(v1, v2)
     m = np.array(((a1, b1), (a2, b2)))
     c = np.array((c1, c2))
-    return np.linalg.slove(m, c)
+    return np.linalg.solve(m, c)
 
 def do_segments_intersect(s1, s2):
     u1, u2 = s1
     v1, v2 = s2
     d1, d2 = distance(*s1), distance(*s2)
-    x, y = intersection(u1, u2, v1, v2)
-    return (
-        distance(u1, (x, y)) < d1 and
-        distance(u2, (x, y)) < d1 and
-        distance(v1, (x, y)) < d2 and
-        distance(v2, (x, y)) < d2
-    )
+    try:
+        x, y = intersection(u1, u2, v1, v2)
+        return (
+            distance(u1, (x, y)) <= d1 and
+            distance(u2, (x, y)) <= d1 and
+            distance(v1, (x, y)) <= d2 and
+            distance(v2, (x, y)) <= d2
+        )
+    except np.linalg.linalg.LinAlgError:
+        return False # 方程无解或有无数多个解
 
 
 def dot(u, v):
