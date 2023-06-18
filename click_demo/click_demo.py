ca26e716c429547e7b6020b80775a846b070722f
Richard Ma <richard.ma.19850509@gmail.com>
2023-06-18 21:37:12 +0800

    add type of parameters

--- click_demo/click_demo.py
+++ click_demo/click_demo.py
@@ -2,7 +2,7 @@ import click
 
 
 @click.command()
-@click.option("-n", "--num", help="input a num")
+@click.option("-n", "--num", type=int, help="input a num")
 def main(num):
     click.echo(f"{num =}")
 
