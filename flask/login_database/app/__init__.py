14c1860138557f32dcf7fe1a43b8f3b7e194731b
richard-ma <richard.ma.19850509@gmail.com>
2023-07-09 21:45:01 +0800

    注册用户

--- flask/login_database/app/__init__.py
+++ flask/login_database/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 
 from app.models import db
 
@@ -8,5 +9,6 @@ app.config['SECRET_KEY'] = '9886f27f2b6c200b6c4b54a8966b9dab'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db.init_app(app)
+bcrypt = Bcrypt(app)
 
 from app import routes
