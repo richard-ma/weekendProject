6a28d701b356ff585dcfc5f1400ad134521b4c17
richard-ma <richard.ma.19850509@gmail.com>
2023-07-10 10:24:42 +0800

    resize avandar

--- flask/login_database/app/routes.py
+++ flask/login_database/app/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
@@ -78,7 +79,12 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename) # split filename to name and ext
     picture_fn = random_hex + f_ext # new filename
     picture_path = os.path.join(app.root_path, 'static', 'profile_pics', picture_fn)
-    form_picture.save(picture_path)
+    # resize picture
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    # save resized picture
+    i.save(picture_path)
 
     return picture_fn
 
