243ed47df1ba23dcd9f5f1847d43b2d007cd3051
richard-ma <richard.ma.19850509@gmail.com>
2023-07-10 21:24:55 +0800

    blog init commit

---
+++ flask/blog_system/app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+
+from app.models import db, login_manager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9886f27f2b6c200b6c4b54a8966b9dab'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db.init_app(app)
+bcrypt = Bcrypt(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from app import routes
