44cb0defe09225533242d30651d1421d48751b10
richard-ma <richard.ma.19850509@gmail.com>
2023-08-12 00:06:08 +0800

    添加testcase

---
+++ cscart_api/tests/test_sender_requests.py
@@ -0,0 +1,23 @@
+import os
+import unittest
+
+from cscartapi.sender_requests import *
+
+
+class TestSenderRequests(unittest.TestCase):
+    def setUp(self):
+        self.base_url = os.environ['CSCART_BASE_URL']
+        self.username = os.environ['CSCART_USERNAME']
+        self.api_key = os.environ['CSCART_API_KEY']
+
+        self.sender = SenderRequests(self.username, self.api_key)
+
+    def tearDown(self):
+        pass
+
+    def test_get(self):
+        url = self.base_url + "users/"
+
+        response = self.sender.get(url)
+        #self.assertTrue(type(response) is dict)
+        self.assertIn('users', response.keys())
