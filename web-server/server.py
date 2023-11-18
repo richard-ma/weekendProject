e33f6c3dfb000846a5075fbf667860c7828f6176
richard-ma <richard.ma.19850509@gmail.com>
2023-11-18 22:54:36 +0800

    get some server parameters

--- web-server/server.py
+++ web-server/server.py
@@ -8,17 +8,38 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     <title>TITLE</title>
 </head>
 <body>
-    <h1>Hello web!</h1>
+    <table>
+        <tr>  <td>Header</td>         <td>Value</td>          </tr>
+        <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
+        <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
+        <tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
+        <tr>  <td>Command</td>        <td>{command}</td>      </tr>
+        <tr>  <td>Path</td>           <td>{path}</td>         </tr>
+    </table>
 </body>
 </html>
 '''
+    def create_page(self):
+        values = {
+            'date_time'   : self.date_time_string(),
+            'client_host' : self.client_address[0],
+            'client_port' : self.client_address[1],
+            'command'     : self.command,
+            'path'        : self.path
+        }
+        page = self.Page.format(**values)
+        return page
+
+    def send_page(self, page):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(page)))
+        self.end_headers()
+        self.wfile.write(bytes(page, 'utf-8'))
 
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(self.Page)))
-        self.end_headers()
-        self.wfile.write(bytes(self.Page, 'utf-8'))
+        page = self.create_page()
+        self.send_page(page)
 
 
 if __name__ == "__main__":
