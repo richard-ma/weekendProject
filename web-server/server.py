d2ac352a3b2ae0650ca8abe57972d4be0898d92a
richard-ma <richard.ma.19850509@gmail.com>
2023-11-19 18:46:28 +0800

    server static files

--- web-server/server.py
+++ web-server/server.py
@@ -1,4 +1,5 @@
 import http.server
+import os
 
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
@@ -19,6 +20,16 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
 </body>
 </html>
 '''
+
+    Error_Page = '''\
+    <html>
+    <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+    </body>
+    </html>
+'''
+
     def create_page(self):
         values = {
             'date_time'   : self.date_time_string(),
@@ -38,8 +49,36 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         self.wfile.write(bytes(page, 'utf-8'))
 
     def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
+        try:
+            full_path = os.getcwd() + self.path
+            if not os.path.exists(full_path):
+                raise ServerException("'{0}' not found.".format(self.path))
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            else:
+                raise ServerException("Unknown object '{0}'".format(self.path))
+        except Exception as msg:
+            self.handle_error(msg)
+
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
+    
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content)
 
 
 if __name__ == "__main__":
