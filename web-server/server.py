e4c302b1195e04706bdd7e1a9eef7fdbfdd92455
richard-ma <richard.ma.19850509@gmail.com>
2023-11-19 19:16:07 +0800

    list files in directory

--- web-server/server.py
+++ web-server/server.py
@@ -2,6 +2,10 @@ import http.server
 import os
 
 
+class ServerException(Exception):
+    pass
+
+
 class case_no_file:
     def test(self, handler):
         return not os.path.exists(handler.full_path)
@@ -38,6 +42,18 @@ class case_directory_index_file:
         handler.handle_file(self.index_path(handler))
 
 
+class case_directory_no_index_file:
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        return os.path.isdir(handler.full_path) and \
+            not os.path.isfile(self.index_path(handler))
+
+    def act(self, handler):
+        handler.list_dir(handler.full_path)
+
+
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     Page = '''\
 <html>
@@ -58,18 +74,27 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
 '''
 
     Error_Page = '''\
-    <html>
-    <body>
-        <h1>Error accessing {path}</h1>
-        <p>{msg}</p>
-    </body>
-    </html>
+<html>
+<body>
+    <h1>Error accessing {path}</h1>
+    <p>{msg}</p>
+</body>
+</html>
+'''
+
+    Listing_Page = '''\
+<html>
+<body>
+    <ul>{0}</ul>
+</body>
+</html>
 '''
 
     Cases = [
         case_no_file,
         case_existing_file,
         case_directory_index_file,
+        case_directory_no_index_file,
         case_always_fail,
     ]
 
@@ -115,7 +140,7 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
 
     def handle_error(self, msg):
         content = self.Error_Page.format(path=self.path, msg=msg)
-        self.send_content(content, 404)
+        self.send_content(bytes(content, 'utf-8'), 404)
     
     def send_content(self, content, status=200):
         self.send_response(status)
@@ -123,6 +148,16 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_header("Content-Length", str(len(content)))
         self.end_headers()
         self.wfile.write(content)
+
+    def list_dir(self, full_path):
+        try:
+            entries = os.listdir(full_path)
+            bullets = ['<li>{0}</li>'.format(e) for e in entries if not e.startswith('.')]
+            page = self.Listing_Page.format('\n'.join(bullets))
+            self.send_content(bytes(page, 'utf-8'))
+        except OSError as msg:
+            msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
+            self.handle_error(msg)
 
 
 if __name__ == "__main__":
