c4085bed3eae5761731ad3b823f56b018489e98f
richard-ma <richard.ma.19850509@gmail.com>
2024-06-20 19:43:03 +0800

    add

---
+++ 202406-4librarySystem/library.py
@@ -0,0 +1,13 @@
+class Library:
+    def __init__(self):
+        self._users = list()
+        self._books = list()
+
+    def load_user(self):
+        pass
+
+    def load_book(self):
+        pass
+
+    def load(self):
+        pass
