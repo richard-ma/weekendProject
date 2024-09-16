3fdc92628aa2a949f88e3f7c357a9f693e0c68a0
richard-ma <richard.ma.19850509@gmail.com>
2024-09-16 21:49:08 +0800

    complete

--- 202409-3PythonModel/facade/facade.py
+++ 202409-3PythonModel/facade/facade.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-class EventManager:
+class EventManager: # 作为统一接口，供You调用的门面
     def __init__(self):
         print("Event Manager:: Let me talk to the folks")
 
@@ -19,6 +19,7 @@ class EventManager:
             self.musician.setMusicType()
 
 
+# 负责预定酒店的负责人
 class Hotelier:
     def __init__(self):
         print("Arranging the Hotel for Marriage? --")
@@ -32,6 +33,7 @@ class Hotelier:
             print("Registered the Booking")
 
 
+# 负责花卉装饰的人员
 class Florist:
     def __init__(self):
         print("Flower Decorations for the Event? --")
@@ -39,6 +41,8 @@ class Florist:
     def setFlowerRequirements(self):
         print("Carnations, Rose and Lilies would be used for Decorations")
 
+
+# 厨师
 class Caterer:
     def __init__(self):
         print("Food Arrangements for the Event")
@@ -46,6 +50,8 @@ class Caterer:
     def setCuisine(self):
         print("Chinese & Continental Cuisine to be served")
 
+
+# 乐手
 class Musician:
     def __init__(self):
         print("Musical Arrangements for the Marriage")
@@ -54,7 +60,19 @@ class Musician:
         print("Jazz and Classical will be played")
 
 
-class You:
+# 调用EventManager的你
+class You(object):
     def __init__(self):
-        print("You:: Whoa! Marriage Arrangements??")
-    
+        print("You:: Whoa! Marriage Arrangements? ? ! ! ! ")
+
+    def askEventManager(self):
+        print("You:: Let's Contact the Event Manager\n\n")
+        em = EventManager()
+        em.arrange()
+
+    def __del__(self):
+        print("You:: Thanks to Event Manager, all preparations done!  Phew! ")
+
+if __name__ == "__main__":
+    you = You()
+    you.askEventManager() # 所有细节都由EventManger来管理安排
