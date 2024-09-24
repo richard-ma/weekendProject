9828a1fb31625e900de0f27a880168d9996f4cf3
richard-ma <richard.ma.19850509@gmail.com>
2024-09-24 22:52:28 +0800

    update

--- 202409-5Python/basic/fraction.py
+++ 202409-5Python/basic/fraction.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+def gcd(m:int, n:int) -> int:
+    while m % n != 0:
+        oldm = m
+        oldn = n
+
+        m = oldn
+        n = oldm % oldn
+    return n
+
 class Fraction:
     def __init__(self, top: int, bottom: int) -> None:
         self.num = top
@@ -16,7 +25,15 @@ class Fraction:
                 self.den * otherfraction.num
         new_den = self.den * otherfraction.den
 
-        return Fraction(new_num, new_den)
+        common = gcd(new_num, new_den)
+
+        return Fraction(new_num // common, new_den // common)
+
+    def __eq__(self, other):
+        firstnum = self.num * other.den
+        secondsum = self.den * other.num
+
+        return firstnum == secondsum
 
 if __name__ == "__main__":
     f = Fraction(3, 5)
@@ -25,3 +42,6 @@ if __name__ == "__main__":
 
     other_f = Fraction(1, 5)
     print(f + other_f)
+
+    other = Fraction(3, 5)
+    print(f == other) # True
