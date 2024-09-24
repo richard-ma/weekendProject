7fec862962775743a4968c02b521637f2e1cc929
richard-ma <richard.ma.19850509@gmail.com>
2024-09-24 23:50:02 +0800

    chapter exercise

--- 202409-5Python/basic/fraction.py
+++ 202409-5Python/basic/fraction.py
@@ -10,9 +10,12 @@ def gcd(m:int, n:int) -> int:
     return n
 
 class Fraction:
+    # 练习1.8 5题：确保分子分母均为整数
     def __init__(self, top: int, bottom: int) -> None:
-        self.num = top
-        self.den = bottom
+    # 练习1.8 2题：在创建分数时约分
+        common = gcd(top, bottom)
+        self.num = top // common
+        self.den = bottom // common
 
     def show(self):
         print(self.num, '/', self.den)
@@ -25,15 +28,66 @@ class Fraction:
                 self.den * otherfraction.num
         new_den = self.den * otherfraction.den
 
-        common = gcd(new_num, new_den)
+        return Fraction(new_num, new_den)
 
-        return Fraction(new_num // common, new_den // common)
+    # 练习1.8 3题：添加方法
+    def __sub__(self, otherfraction):
+        new_num = self.num * otherfraction.den - \
+                self.den * otherfraction.num
+        new_den = self.den * otherfraction.den
 
-    def __eq__(self, other):
+        return Fraction(new_num, new_den)
+
+    def __mul__(self, otherfraction):
+        new_num = self.num * otherfraction.num
+        new_den = self.den * otherfraction.den
+
+        return Fraction(new_num, new_den)
+
+    def resiprocal(self): # 将本分数变为倒数
+        return Fraction(self.den, self.num)
+
+    def __truediv__(self, otherfraction):
+        return self.__mul__(otherfraction.resiprocal())
+
+    def getFristSecond(self, other):
         firstnum = self.num * other.den
         secondsum = self.den * other.num
 
+        return (firstnum, secondsum)
+
+    def __eq__(self, other):
+        firstnum, secondsum = self.getFristSecond(other)
         return firstnum == secondsum
+
+    # 练习1.8 4题：添加__ne__, __gt__, __lt__, __ge__, __le__方法
+
+    def __ne__(self, other):
+        firstnum, secondsum = self.getFristSecond(other)
+        return firstnum != secondsum
+
+    def __gt__(self, other):
+        firstnum, secondsum = self.getFristSecond(other)
+        return firstnum > secondsum
+
+    def __ge__(self, other):
+        firstnum, secondsum = self.getFristSecond(other)
+        return firstnum >= secondsum
+
+    def __lt__(self, other):
+        firstnum, secondsum = self.getFristSecond(other)
+        return firstnum < secondsum
+
+    def __le__(self, other):
+        firstnum, secondsum = self.getFristSecond(other)
+        return firstnum <= secondsum
+
+    # 练习1.8 1题：添加getNum和getDen方法
+    def getNum(self):
+        return self.num
+
+    def getDen(self):
+        return self.den
 
 if __name__ == "__main__":
     f = Fraction(3, 5)
@@ -43,5 +97,15 @@ if __name__ == "__main__":
     other_f = Fraction(1, 5)
     print(f + other_f)
 
+    print(f - other_f)
+    print(f * other_f)
+    print(f / other_f)
+
     other = Fraction(3, 5)
     print(f == other) # True
+    print(other_f != other) # True
+    print(f != other) # False
+    print(f > other_f) # True
+    print(other_f < f) # True
+    print(f >= f) # True
+    print(f <= f) # True
