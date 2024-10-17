e6d2c8a47e007760c176ad3f3261a765957d74a0
richard-ma <richard.ma.19850509@gmail.com>
2024-10-17 16:00:02 +0800

    add

---
+++ 202409-5Python/parse_tree.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+
+from linear_structures.linear_stack import Stack
+from tree.binaryTree import BinaryTree
+
+def buildParseTree(fpexp):
+    fplist = fpexp.split()
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+
+        elif i in ['+', '-', '*', '/']:
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+
+        elif i == ')':
+            currentTree = pStack.pop()
+
+        elif i not in ['+', '-', '*', '/', ')']:
+            try:
+                currentTree.setRootVal(int(i))
+                parent = pStack.pop()
+                currentTree = parent
+
+            except ValueError:
+                raise ValueError("token '{}' is not a valid integer".format(i))
+
+    return eTree
+
+def preorder(tree):
+    if tree:
+        print(tree.getRootVal())
+        preorder(tree.getLeftChild())
+        preorder(tree.getRightChild())
+
+def postorder(tree):
+    if tree:
+        preorder(tree.getLeftChild())
+        preorder(tree.getRightChild())
+        print(tree.getRootVal())
+
+def inorder(tree):
+    if tree:
+        preorder(tree.getLeftChild())
+        print(tree.getRootVal())
+        preorder(tree.getRightChild())
+
+def postordereval(tree):
+    opers = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.truediv}
+    res1 = None
+    res2 = None
+    if tree:
+        res1 = postordereval(tree.getLeftChild())
+        res2 = postordereval(tree.getRightChild())
+        if res1 and res2:
+            return opers[tree.getRootVal()](res1,res2)
+        else:
+            return tree.getRootVal()
+
+def printexp(tree):
+    sVal = ""
+    if tree:
+        sVal = '(' + printexp(tree.getLeftChild())
+        sVal = sVal + str(tree.getRootVal())
+        sVal = sVal + printexp(tree.getRightChild())+')'
+    return sVal
+
+
+if __name__ == "__main__":
+    pt = buildParseTree("( ( 10 + 5 ) * 3 )")
+    postorder(pt)  #defined and explained in the next section
