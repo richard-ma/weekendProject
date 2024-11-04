cb822648e9e391eeba2737bee54b7aeab511c907
richard-ma <richard.ma.19850509@gmail.com>
2024-11-04 11:36:07 +0800

    init commit

--- 202411-1DateCounter/datecounter.py
+++ 202411-1DateCounter/datecounter.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import json
 from datetime import datetime, timedelta, date
+from configparser import ConfigParser
 
 def date_range(start, end, periods=1):
     start_time = date.fromisoformat(start)
@@ -13,5 +15,38 @@ def date_range(start, end, periods=1):
         yield current_time
         current_time += interval
 
-for d in date_range('2024-10-01', '2024-11-01'):
-    print(d, d.weekday())
+# for d in date_range('2024-10-01', '2024-11-01'):
+#     print(d, d.weekday(d))
+
+def get_list(item_obj):
+    return json.loads(item_obj)
+
+
+if __name__ == "__main__":
+    schedule_filename = './schedule.ini'
+    schedule = ConfigParser()
+
+    schedule.read(schedule_filename)
+
+    ans = dict()
+    schedule_table = dict()
+
+    for section in schedule.sections():
+        section_name = schedule.get(section, "name")
+        section_schedule = get_list(schedule.get(section, "schedule"))
+        schedule_table[section] = section_schedule
+
+    for d in date_range('2024-10-01', '2024-11-01'):
+        key = d.weekday()
+
+        for k, v in schedule_table.items():
+            if key > 4:
+                continue # Sat Sun
+
+            if v[key] in ans.keys():
+                ans[v[key]].append(k)
+            else:
+                ans[v[key]] = [k]
+
+    print(schedule_table)
+    print(ans)
