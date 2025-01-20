af2a860015ce47972770264fb0004bccccdeb9d7
richard-ma <richard.ma.19850509@gmail.com>
2025-01-20 21:39:55 +0800

    add first test

---
+++ 202501-4MazesForProgrammers/tests/test_true.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TrueTest(unittest.TestCase):
+    def setUp(self):
+        return super().setUp()
+    
+    def tearDown(self):
+        return super().tearDown()
+    
+    def test_testrunner(self):
+        return True
