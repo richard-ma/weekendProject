e00ded4e3ced173bdde3c1698cf6463310f67008
richard-ma <richard.ma.19850509@gmail.com>
2025-01-21 13:37:42 +0800

    add sidewinder testcase

---
+++ 202501-4MazesForProgrammers/tests/test_sidewinder.py
@@ -0,0 +1,31 @@
+import unittest
+from src.sidewinder import *
+from src.grid import *
+
+
+class TestSidewinder(unittest.TestCase):
+    def setUp(self):
+        return super().setUp()
+    
+    def tearDown(self):
+        return super().tearDown()
+    
+    def test_binary_tree_on(self):
+        g = Grid(4, 4)
+
+        link_count = 0
+        for cell in g.each_cell():
+            if len(cell.links()) > 0:
+                link_count += len(cell.links())
+        self.assertEqual(link_count, 0)
+
+        sw = Sidewinder()
+        sw.on(g)
+
+        link_count = 0
+        for cell in g.each_cell():
+            if len(cell.links()) > 0:
+                link_count += len(cell.links())
+
+        self.assertGreater(link_count, 0)
+        # print(g) # show grid
