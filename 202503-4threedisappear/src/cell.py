d8d0d26887d8c9e33f1da5c24e530603a0f7bbd9
richard-ma <richard.ma.19850509@gmail.com>
2025-03-19 20:52:38 +0800

    add cell

--- 202503-4threedisappear/src/cell.py
+++ 202503-4threedisappear/src/cell.py
@@ -1,6 +1,27 @@
 class Cell:
-    def __init__(self, row: int, column: int, data: int):
-        self._row = row
-        self._column = column
+    def __init__(self, value: int):
+        self._value = value 
 
-        self._data = data
+    def value(self, value: int = None):
+        if value is not None:
+            self._value = value
+
+        return self._value
+    
+    def __eq__(self, value):
+        return self._value == value
+
+    def __ne__(self, value):
+        return self._value != value
+
+    def __gt__(self, value):
+        return self._value > value
+
+    def __ge__(self, value):
+        return self._value >= value
+    
+    def __lt__(self, value):
+        return self._value < value
+    
+    def __le__(self, value):
+        return self._value <= value
