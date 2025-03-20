bdfbfc119b1db63c721a04fdf61aa5aa632c06b8
richard-ma <richard.ma.19850509@gmail.com>
2025-03-20 09:00:19 +0800

    update

--- 202503-4threedisappear/src/cell.py
+++ 202503-4threedisappear/src/cell.py
@@ -1,6 +1,9 @@
 class Cell:
-    def __init__(self, value: int):
-        self._value = value 
+    def __init__(self, value: int = None):
+        if value is None:
+            self._value = 0
+        else:
+            self._value = value 
 
     def value(self, value: int = None):
         if value is not None:
