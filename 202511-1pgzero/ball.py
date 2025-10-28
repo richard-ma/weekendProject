fc7993e22b036316b4c1e40931b055d328929765
richard-ma <richard.ma.19850509@gmail.com>
2025-10-28 10:41:06 +0800

    add ball game

--- 202511-1pgzero/ball.py
+++ 202511-1pgzero/ball.py
@@ -1,6 +1,29 @@
+import random
+
 WIDTH = 500
 HEIGHT = 300
 
+NUM = 10
+balls = list()
+
+for i in range(NUM):
+    ball = Actor("breakout_ball", (200, 100))
+    ball.x = random.randint(0, WIDTH)
+    ball.y = random.randint(0, HEIGHT)
+    ball.dx = 5
+    ball.dy = 5
+    balls.append(ball)
+
 def draw():
     screen.fill((255, 255, 255))
-    screen.blit("breakout_ball", (200, 100))
+    for ball in balls:
+        ball.draw()
+
+def update():
+    for ball in balls:
+        ball.x += ball.dx
+        ball.y += ball.dy
+        if ball.right > WIDTH or ball.left < 0:
+            ball.dx = -ball.dx
+        if ball.bottom > HEIGHT or ball.top < 0:
+            ball.dy = -ball.dy
