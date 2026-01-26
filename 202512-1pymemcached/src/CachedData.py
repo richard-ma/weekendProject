1f7b8a35c21cd064392cc4147ba72b0f61f28171
richard-ma <richard.ma.19850509@gmail.com>
2026-01-26 09:47:11 +0800

    add delete exists keys methods

--- 202512-1pymemcached/src/CachedData.py
+++ 202512-1pymemcached/src/CachedData.py
@@ -7,3 +7,13 @@ class CachedData:
 
     def get(self, key):
         return self.store.get(key, None)
+
+    def delete(self, key):
+        if key in self.store:
+            del self.store[key]
+    
+    def exists(self, key):
+        return key in self.store
+    
+    def keys(self):
+        return list(self.store.keys())
